14f0ea05ad8fe2cdd3499206778fe6b6cadefa14
arunpatil <arunpatilmailbox@gmail.com>
2023-01-26 21:00:59 -0700

    Added github pipeline and configuration setup

--- src/main/java/com/ap/myshoppingappconfigservice/MyShoppingappConfigserviceApplication.java
+++ src/main/java/com/ap/myshoppingappconfigservice/MyShoppingappConfigserviceApplication.java
@@ -2,8 +2,10 @@ package com.ap.myshoppingappconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MyShoppingappConfigserviceApplication {
 
 	public static void main(String[] args) {
